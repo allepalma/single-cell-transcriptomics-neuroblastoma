da0d9d00a6f32256f385c9dedd37b1f516240223
Alessandro <65178831+allepalma@users.noreply.github.com>
2020-09-07 16:13:29 +0200

    Add files via upload

--- R/004.Cell_cycle_colouring.R
+++ R/004.Cell_cycle_colouring.R
@@ -15,11 +15,6 @@ k_rc <- rawcounts
 load('be2c-rawcounts.rda')
 b_rc <- rawcounts
 
-#Remove genes that are not expressed in both cells
-keep <- !(apply(k_rc,1,sum)<=0 & apply(b_rc,1,sum)<=0)
-k_rc <- k_rc[keep,]
-b_rc <- b_rc[keep,]
-
 #The mitigation of the effects of the cell cycle on the expression counts is performed 
 #treating the two datasets separately.
 
@@ -31,7 +26,7 @@ g2m.genes <- cc.genes$g2m.genes
 #Do separate cell cycle regression analysis
 k_seu <- CreateSeuratObject(k_rc)
 k_seu <- NormalizeData(k_seu)
-k_seu <- FindVariableFeatures(k_seu, selection.method = "vst",nfeatures = 10000)
+k_seu <- FindVariableFeatures(k_seu, selection.method = "vst",nfeatures = 5000)
 k_seu <- ScaleData(k_seu, features = rownames(k_seu))
 #Assign to each cell a cell cycle score for s and g2
 k_seu <- CellCycleScoring(k_seu, s.features = s.genes, 
@@ -55,14 +50,14 @@ dev.off()
 #Repeat the pipeline for BE(2)C cells.
 b_seu <- CreateSeuratObject(b_rc)
 b_seu <- NormalizeData(b_seu)
-b_seu <- FindVariableFeatures(b_seu, selection.method = "vst",nfeatures = 10000)
+b_seu <- FindVariableFeatures(b_seu, selection.method = "vst",nfeatures = 5000)
 b_seu <- ScaleData(b_seu, features = rownames(b_seu))
 b_seu <- CellCycleScoring(b_seu, s.features = s.genes, 
                           g2m.features = g2m.genes, set.ident = TRUE)
-PCA2_b <- RunPCA(b_seu, features = VariableFeatures(b_seu))
+PCA_b <- RunPCA(b_seu, features = VariableFeatures(b_seu))
 
 png('BE2C_before_regression.png',width=700,height=700, res=120)
-plot <- DimPlot(PCA2_b,pt.size = 2)
+plot <- DimPlot(PCA_b,pt.size = 2)
 plot + ggtitle('Gene expression before regression in BE(2)C cells')+theme(plot.title = element_text(hjust = 0.5))
 dev.off()
 
@@ -80,16 +75,16 @@ phases_k <- k_seu$Phase
 phases_b <- b_seu$Phase
 perc_k <- table(phases_k)/sum(table(phases_k))*100
 perc_b <- table(phases_b)/sum(table(phases_b))*100
-perc_b[1]<-37
 
-png(file='cell-cycle_phases.png',width = 700,height=700, res=100)
+
+png(file='cell-cycle_phases.png',width = 700,height=700, res=90)
 par(mfrow=c(1,2))
-k <-plot(phases_k, col=c('coral','chartreuse3','cyan3'),main='KELLY cells phases',ylim=c(0,500),ylab='Number of cells'
+k <-plot(phases_k, col=c('coral','chartreuse3','cyan3'),main='KELLY cells phases',ylim=c(0,600),ylab='Number of cells'
          ,cex.lab=1.5,cex.main=1.5,font=2)
-text(k,table(phases_k)+50,labels =paste0(round(perc_k),rep('%',3)),font=2)
-b <- plot(phases_b, col=c('coral','chartreuse3','cyan3'),main='BE(2)-C cells phases',ylim=c(0,500),ylab='Number of cells'
+text(k,table(phases_k)+20,labels =paste0(round(perc_k),rep('%',3)),font=2)
+b <- plot(phases_b, col=c('coral','chartreuse3','cyan3'),main='BE(2)-C cells phases',ylim=c(0,600),ylab='Number of cells'
           ,cex.lab=1.5,cex.main=1.5,font=2)
-text(b,table(phases_b)+50,labels =paste0(round(perc_b),rep('%',3)),font=2)
+text(b,table(phases_b)+20,labels =paste0(round(perc_b),rep('%',3)),font=2)
 dev.off()
 
 
@@ -109,7 +104,6 @@ col <- ifelse(names(PC1)%in%names(cluster1),'cornflowerblue','chartreuse3')
 png('PCA_sep_be2c_after_reg.png',width=700,height=700)
 plot(PC1,PC2,pch=20,col=col,xlab ='PC1',ylab='PC2',main='Clusters of SK-N-BE(2)-C after cell cycle regression',
      cex.lab=1.5,font.lab=2, font=2,cex.main=2.2,cex=2)
-text(PC1[name],PC2[name],labels = name,font = 2)
 legend('topright',legend = c('group1','group2'),pch=20,col=c('cornflowerblue','chartreuse3'),cex=2)
 dev.off()
 
